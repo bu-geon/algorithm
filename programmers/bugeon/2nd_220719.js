eb96f069345799bf93af160f73f1268ed3c87d3e
bugeon <bearbu@naver.com>
2022-07-20 19:29:15 +0900

    2번 문제 할인 만족하는 조건 O(1) 시간 복잡도로 변경

--- programmers/bugeon/2nd_220719.js
+++ programmers/bugeon/2nd_220719.js
@@ -7,25 +7,38 @@ function solution(want, number, discount) {
 		return acc;
 	}, {});
 
+	let targetCnt = 0;
+
 	const periodNumber = Array(number.length).fill(0);
 	for (let i = 0; i < 10; i++) {
 		if (Object.keys(wantObj).includes(discount[i])) {
+			if (number[wantObj[discount[i]]] > periodNumber[wantObj[discount[i]]]) {
+				targetCnt++;
+			}
 			periodNumber[wantObj[discount[i]]]++;
 		}
 	}
 
-	if (number.every((count, i) => count === periodNumber[i])) answer++;
+	// if (number.every((count, i) => count === periodNumber[i])) answer++;
+	if (targetCnt === 10) answer++;
 
 	for (let i = 10; i < discount.length; i++) {
 		if (Object.keys(wantObj).includes(discount[i])) {
+			if (number[wantObj[discount[i]]] > periodNumber[wantObj[discount[i]]]) {
+				targetCnt++;
+			}
 			periodNumber[wantObj[discount[i]]]++;
 		}
 
 		if (Object.keys(wantObj).includes(discount[i - 10])) {
+			if (number[wantObj[discount[i]]] >= periodNumber[wantObj[discount[i]]]) {
+				targetCnt--;
+			}
 			periodNumber[wantObj[discount[i - 10]]]--;
 		}
 
-		if (number.every((count, i) => count === periodNumber[i])) answer++;
+		// if (number.every((count, i) => count === periodNumber[i])) answer++;
+		if (targetCnt === 10) answer++;
 	}
 
 	return answer;
