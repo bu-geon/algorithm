3acf6be10c1c44ee21342153e5d915144e58a600
yhnb3 <60080270+yhnb3@users.noreply.github.com>
2022-07-20 23:58:47 +0900

    Fix: leetcode

--- programmers/2022mock_exam/엄강우/2회/leetcode.py
+++ programmers/2022mock_exam/엄강우/2회/leetcode.py
@@ -1,6 +1,14 @@
 class Solution:
     def longestSubsequence(self, s: str, k: int) -> int:
-        while int(s, 2) > k:
-            s = s.replace("1", "", 1)
+        answer = 0
+        value = 0
+        n = len(s)
+        for idx in range(n-1, -1, -1):
+            if s[idx] == "1":
+                if value + 2 ** (n-idx-1) <= k:
+                    value += 2 ** (n-idx-1)
+                    answer += 1
+            else:
+                answer += 1
         
-        return len(s)
+        return answer
