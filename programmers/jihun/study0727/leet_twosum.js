ed3681d42f306303e69bd206a9fd3a1bd4f0182d
jihun1233 <leejihun1233@gmail.com>
2022-07-28 16:17:37 +0900

    세가지 수 경우

--- programmers/jihun/study0727/leet_twosum.js
+++ programmers/jihun/study0727/leet_twosum.js
@@ -3,7 +3,7 @@
  * @param {number} target
  * @return {number[]}
  */
- var twoSum = function(nums, target) {
+ var twoSum = function(nums, target, ignoreIndex) {
     const numsObj = nums.reduce((acc,num,index) => {
       if(!acc[num]){
         acc[num] = {count: 1, index: [index]}
@@ -17,23 +17,37 @@
 
     let result = []
 
-    for (let index = 0; index < nums.length; index++) {
+    for (let index = ignoreIndex + 1; index < nums.length; index++) {
       const num = nums[index];
       const restNum = target - num
       if(!numsObj[restNum])continue
 
-      console.log(num, restNum)
-      if(restNum === num){
-        if(numsObj[num].count === 1)continue
-        result.push(numsObj[num].index[0], numsObj[num].index[1])
-      }else{
-        result.push(numsObj[num].index[0], numsObj[restNum].index[0])
-      }
+      result.push(numsObj[num].index[0], numsObj[restNum].index[0])
+      
       return result
     }
+
+    return false
 };
 
+const threeSum = (nums, target) => {
+  let results = []
+
+  for (let index = 0; index < nums.length; index++) {
+    const restTarget = target - nums[index]
+    const restSum = twoSum(nums, restTarget, index)
+
+    if(restSum)results.push([...restSum, index])
+
+  }
+  return results
+}
+
+// console.log(twoSum(
+//   // [2,7,11,15], 9
+//   [3,2,4], 6
+// ))
+
 console.log(twoSum(
-  // [2,7,11,15], 9
-  [3,2,4], 6
+  [1,2,3,4,5,4,3,2,1], 6 
 ))
