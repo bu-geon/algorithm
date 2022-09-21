40765a9221ee01c0040bdb86b32ab1f06def890c
bugeon <bearbu@naver.com>
2022-09-21 12:38:58 +0900

    refactor: Refactoring 1838, 1884

--- programmers/김부건/leetcode/1884.js
+++ programmers/김부건/leetcode/1884.js
@@ -3,15 +3,14 @@
  * @return {number}
  */
 const twoEggDrop = (n) => {
-	const dp = [{ one: 0, two: 0 }];
+	const dp = { one: 0, two: 0 };
 
 	let i = 1;
-	while (dp[i - 1].two < n) {
-		dp.push({ one: i, two: dp[i - 1].one + dp[i - 1].two + 1 });
+	while (dp.two < n) {
+		dp.two = dp.two + dp.one + 1;
+		dp.one = i;
 		i++;
 	}
 
 	return i - 1;
 };
-
-console.log(twoEggDrop(100));
