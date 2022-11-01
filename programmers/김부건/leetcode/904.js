0529b21332e03309b124a595d47a058abf3178e6
bugeon <bearbu@naver.com>
2022-11-01 14:53:44 +0900

    leetcode: update solution 904. Fruit Into Baskets

--- programmers/김부건/leetcode/904.js
+++ programmers/김부건/leetcode/904.js
@@ -22,3 +22,30 @@ var totalFruit = function (tree) {
 
   return maxFruits;
 };
+
+// var totalFruit = function (tree) {
+//   let [first, second, third] = [undefined, undefined, undefined];
+
+//   let [left, maxFruits] = [0, 0];
+//   for (let right = 0; right < tree.length; right++) {
+//     const fruit = tree[right];
+
+//     if (first === undefined || fruit === first.fruit) {
+//       first = { fruit: fruit, lastIndex: right };
+//     } else if (second === undefined || fruit === second.fruit) {
+//       second = { fruit: fruit, lastIndex: right };
+//     } else {
+//       if (first.lastIndex >= second.lastIndex) {
+//         left = second.lastIndex + 1;
+//         second = { fruit: fruit, lastIndex: right };
+//       } else {
+//         left = first.lastIndex + 1;
+//         first = { fruit: fruit, lastIndex: right };
+//       }
+//     }
+
+//     maxFruits = Math.max(maxFruits, right - left + 1);
+//   }
+
+//   return maxFruits;
+// };
