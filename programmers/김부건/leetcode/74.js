d461915e832660fde55a4453bab41aa7da994ba6
bugeon <bearbu@naver.com>
2022-11-09 01:54:57 +0900

    leetcode: add 74. Search a 2D Matrix

---
+++ programmers/김부건/leetcode/74.js
@@ -0,0 +1,34 @@
+// 74. Search a 2D Matrix
+var searchMatrix = function (matrix, target) {
+  let [top, bottom] = [0, matrix.length - 1];
+  while (top <= bottom) {
+    const midRow = parseInt((top + bottom) / 2);
+
+    if (target < matrix[midRow][0]) {
+      bottom = midRow - 1;
+    } else if (target > matrix[midRow].at(-1)) {
+      top = midRow + 1;
+    } else {
+      return matrix[midRow].includes(target);
+    }
+  }
+};
+
+//   return false;
+// };
+
+// var searchMatrix = function (matrix, target) {
+//   let [row, col] = [0, matrix[0].length - 1];
+
+//   while (row < matrix.length && col >= 0) {
+//     if (matrix[row].at(-1) < target) {
+//       row++;
+//       continue;
+//     }
+
+//     if (matrix[row][col] === target) return true;
+//     col--;
+//   }
+
+//   return false;
+// };
