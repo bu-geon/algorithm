321104cf744d413befeb45c77f8501636c327b3a
yhnb3 <60080270+yhnb3@users.noreply.github.com>
2022-12-20 22:30:54 +0900

    Add 2 prob

--- programmers/엄강우/20221220.py
+++ programmers/엄강우/20221220.py
@@ -15,3 +15,34 @@ class Solution:
                     start = i
                 i += 1
         return s[start:start+maxLength]
+
+import heapq
+
+class Solution:
+    def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
+        answer = [0 for _ in range(len(temperatures))]
+        h = []
+        for idx, temp in enumerate(temperatures):
+            while h and h[0][0] < temp:
+                prevTemp, prevIdx = heapq.heappop(h)
+                answer[prevIdx] = idx - prevIdx
+            heapq.heappush(h, [temp, idx])
+        return answer
+
+class Solution:
+    def asteroidCollision(self, asteroids: List[int]) -> List[int]:
+        stack = []
+        for ast in asteroids:
+            if ast > 0:
+                stack.append(ast)
+            else:
+                while stack and stack[-1] > 0:
+                    newAst = stack.pop()
+                    if newAst + ast == 0:
+                        break
+                    elif newAst + ast > 0:
+                        stack.append(newAst)
+                        break
+                else:
+                    stack.append(ast)
+        return stack 
